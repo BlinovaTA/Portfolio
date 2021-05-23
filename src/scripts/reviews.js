a8578782bc93a5a61e1cb56a52b2f254a5415434
BlinovaTA <tanyushablinova@gmail.com>
2021-05-24 00:26:39 +0500

    Добавила блокировку кнопок управления в отзывах

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -12,11 +12,15 @@ new Vue({
   data() {
     return {
       reviews: [],
+      disabledNext: false,
+      disabledPrev: true,
       sliderOptions: {
         slidesPerView: 1,
+        slidesPerGroup: 1,
         breakpoints: {
           481: {
-            slidesPerView: 2
+            slidesPerView: 2,
+            slidesPerGroup: 2,
           }
         }
       }
@@ -45,10 +49,24 @@ new Vue({
 
           break;
       }
+    },
+    checkArrows(e) {
+      this.disabledNext = this.$refs.slider.$swiper.activeIndex === this.$refs.slider.$swiper.slides.length - this.$refs.slider.$swiper.params.slidesPerView;
+      this.disabledPrev = this.$refs.slider.$swiper.activeIndex === 0;
+
+      console.log(this.$refs.slider.$swiper)
     }
   },
   created() {
     const data = require("../data/reviews.json");
     this.reviews = this.requireImagesToArray(data);
+  },
+  mounted() {
+    const context = this;
+
+    this.$refs.slider.$swiper.on("slideChange", function (swiper) {
+      context.disabledNext = swiper.activeIndex === swiper.slides.length - swiper.params.slidesPerView;
+      context.disabledPrev = swiper.activeIndex === 0;
+    })
   }
 })
