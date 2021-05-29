b0264c99e8f885552c64cc48fb395891cf5532ee
BlinovaTA <tanyushablinova@gmail.com>
2021-05-29 13:33:24 +0500

    Компонент skill

---
+++ src/admin/components/skill/skill.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  .skill-component(v-if="!editmode")
+    .title Git
+    .percent 80%
+    .buttons
+      icon(
+        symbol="pencil"
+        class="btn"
+        grayscale
+      )
+      icon(
+        symbol="trash"
+        class="btn"
+        grayscale
+      )
+
+  .skill-component(v-else)
+    .title
+      app-input(noSidePaddings)
+    .percent
+      app-input(
+        type="number"
+        min="0"
+        max="100"
+        maxlength="3"
+      )
+    .buttons
+      icon(
+        symbol="tick"
+        class="btn"
+      )
+      icon(
+        symbol="cross"
+        class="btn"
+      )
+</template>
+
+<script>
+import input from "../input";
+import icon from "../icon";
+
+export default {
+  components: {
+    appInput: input,
+    icon  
+  },
+  data() {
+    return {
+      editmode: false
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped src="./skill.pcss"></style>
