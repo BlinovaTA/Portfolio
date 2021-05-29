d09b71fb2ad88d2083b4793e441c70058dd66c65
BlinovaTA <tanyushablinova@gmail.com>
2021-05-29 11:40:07 +0500

    Добавила хедер в приложение и подключила стили

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,26 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  .app-container
+    headline
+      user
 </template>
+
+<script>
+import headline from "./components/headline";
+import user from "./components/user";
+
+export default {
+  components: {
+    headline,
+    user
+  },
+}
+</script>
+
+<style lang="postcss">
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800");
+@import "normalize.css";
+@import "../styles/mixins.pcss";
+@import "../styles/layout/base.pcss";
+</style>
+
+<style lang="postcss" scoped src="./app.pcss"></style>
