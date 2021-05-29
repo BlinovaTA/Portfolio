4164b98beed4ab8d3533087851af44f0db4a5a92
BlinovaTA <tanyushablinova@gmail.com>
2021-05-29 11:41:01 +0500

    Компонент navigation

---
+++ src/admin/components/navigation/navigation.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  nav.navigation-component
+    ul.list
+      li.item(
+        v-for="link in links" 
+        :key="link.id"
+      )
+        a(
+          :href="`/${link.alias}`"
+          :class="['link', {active: link.active}]"
+        ) {{link.title}}
+</template>
+
+<script>
+const links = [
+  {id: 0, title: "Обо мне", alias: "about", active: false},
+  {id: 0, title: "Работы", alias: "works", active: true},
+  {id: 0, title: "Отзывы", alias: "reviews", active: false}
+];
+
+export default {
+  data() {
+    return {
+      links
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped src="./navigation.pcss"></style>
