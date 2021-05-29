a9de9805a2e5edbed2db3e464584cfa4bd0610b3
BlinovaTA <tanyushablinova@gmail.com>
2021-05-29 12:24:05 +0500

    Компонент category

---
+++ src/admin/components/category/category.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  card(slim)
+    edit-line(
+      slot="title"
+      v-model="title"
+      editModeByDefault
+    )
+  
+</template>
+
+<script>
+import card from "../card";
+import editLine from "../editLine";
+
+export default {
+  components: {
+    card,
+    editLine
+  },
+  data() {
+    return {
+      title: ""   
+    }
+  }
+}
+</script>
