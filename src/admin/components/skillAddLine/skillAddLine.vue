bcabe66e3287b7b13b987490e9415956e8ef5b07
BlinovaTA <tanyushablinova@gmail.com>
2021-05-31 17:31:26 +0500

    Валидация поля с новым навыком

--- src/admin/components/skillAddLine/skillAddLine.vue
+++ src/admin/components/skillAddLine/skillAddLine.vue
@@ -3,6 +3,8 @@
     .title
       app-input(
         placeholder="Новый навык"
+        :errorMessage="titleError"
+        v-model="skill.title"
       )
     .percent
       app-input(
@@ -10,10 +12,12 @@
         min="0" 
         max="100" 
         maxlength="3"
+        v-model="skill.percent"
       )
     .button
       round-button(
         type="round"
+        @click="addSkill"
       )
 </template>
 
@@ -28,6 +32,34 @@ export default {
   components: {
     appInput: input,
     roundButton: button,
+  },
+  data() {
+    return {
+      titleError: "",
+      skill: {
+        title: "",
+        percent: 0
+      }
+    }
+  },
+  methods: {
+    addSkill() {
+      this.titleError = "";
+    
+      if (this.skill.title.trim() === "") {
+        this.titleError = "Заполните поле";
+
+        return;
+      }
+
+      if (this.skill.percent === "") {
+        this.skill.percent = 0;
+
+        return;
+      }
+
+      this.$emit('add-skill', this.skill);
+    }
   }
 };
 </script>
