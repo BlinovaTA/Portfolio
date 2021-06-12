4b12c48b8db9dba09ec7830ca08802ca32b293c6
BlinovaTA <tanyushablinova@gmail.com>
2021-06-12 12:56:16 +0500

    Проверка пользователя

--- src/admin/router.js
+++ src/admin/router.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "./store";
+import axios from "axios";
 
 Vue.use(VueRouter);
 
@@ -42,9 +44,32 @@ const routes = [
 
 const router = new VueRouter({ routes });
 
-router.beforeEach((to, from,next) => {
+const guard = axios.create({
+  baseURL: "https://webdev-api.loftschool.com"
+});
+
+router.beforeEach(async (to, from,next) => {
   const isPublicRoute = to.matched.some(route => route.meta.public);
-  next();
+  const isUserLoggedIn = store.getters["user/userIsLoggedIn"];
+
+  if (!isPublicRoute && !isUserLoggedIn) {
+    const token = localStorage.getItem("token");
+
+    guard.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+    try {
+      const response = await guard.get("/user"); 
+      
+      store.dispatch("user/login", await response.data.user);
+
+      next();
+    } catch (error) {
+      router.replace("/login");
+      localStorage.removeItem("token"); 
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
