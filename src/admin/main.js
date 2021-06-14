21ff069d818ddaf62901759b2cd9d719f81f375e
BlinovaTA <tanyushablinova@gmail.com>
2021-06-15 00:20:29 +0500

    Установка baseURL при обновлении страницы

--- src/admin/main.js
+++ src/admin/main.js
@@ -3,12 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import VueSimpleValidator from "simple-vue-validator";
 import store from "./store";
-import $axios from "axios";
-import config from "../../env.paths.json";
+import $axios from "./requests";
 
 Vue.use(VueSimpleValidator, { mode: "manual" });
 
-$axios.defaults.baseURL = config.BASE_URL;
 store.$axios = $axios;
 
 new Vue({
